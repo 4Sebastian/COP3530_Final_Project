98c705ef0fb799831451a15bae7d3634b21b70d6
Sebastian Paulis <sebastian4paulis@gmail.com>
2022-11-18 19:58:20 -0500

    combining noise with graph functionality

--- visualization.py
+++ visualization.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from mpl_toolkits import mplot3d
+from noise import NoiseHandler
+mpl.use('macosx')
 
 #Visualization of Line
 axis = plt.axes(projection="3d")
@@ -17,10 +20,15 @@ x_data = np.arange(0, 5, (0.1))
 y_data = np.arange(0, 5, (0.1))
 z_data = np.arange(0, 5, (0.1))
 
+noise_handler = NoiseHandler()
+noise_handler.genTer(100, (1.0, 3), (0.5, 5))
+x_data, y_data, Z = noise_handler.convert_matplotlib()
+Z = np.array(Z)
+
 #print (x_data)
 X, Y = np.meshgrid(x_data, y_data)
 #print(X)
-Z = X * Y
+#Z = X * Y
 
 
 axis.plot_surface(X, Y, Z)
